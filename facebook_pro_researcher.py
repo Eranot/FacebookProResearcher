f01e2b76065c907ea3ddc55292e7a49eb06d3095
cristianosalvagni <cristiano.salvagni@unochapeco.edu.br>
2018-05-10 18:15:07 -0300

    Added commands

--- facebook_pro_researcher.py
+++ facebook_pro_researcher.py
@@ -6,23 +6,29 @@ import re
 import time
 import os
 
+browser = 0
+
+def openTab(url):
+	browser.execute_script("window.open('" + url + "','_blank');")
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--disable-notifications")
 
-#chrome_options.add_experimental_option("disable-notifications", True)
 
-browser = Browser('chrome', options=chrome_options)
-# Visit URL
-url = "http://www.facebook.com/login"
-browser.visit(url)
+
+global_list = []
 
 while(True):
-	op = input("facebook_pro_researcher# ")
+	command = input("facebook_pro_researcher# ")
+	command += " "
+	
+	c = re.search("(.+?) (.*)", command).groups()
+	op = c[0]
+	args = c[1].split(" ")[0:-1]
 
 	if(op == "save"):
-		currentPage = re.search('https://www.facebook.com/(.+)', browser.url).group(1)
-		browser.visit('https://www.facebook.com/' + currentPage + "/friends")
+		currentPage = re.search('https://www.facebook.com/(.+)[&/]', browser.url).group(1)
+
 		scrollHeight = browser.evaluate_script("document.body.scrollHeight")
 		scrollTop = browser.evaluate_script("document.documentElement.scrollTop")
 		innerHeight = browser.evaluate_script("window.innerHeight")
@@ -42,7 +48,7 @@ while(True):
 			a = f.find_by_tag("a")
 			friend = re.search('https://www.facebook.com/(.+?)\?', a['href']).group(1)
 			if(friend == "profile.php"):
-				friend = re.search('https://www.facebook.com/profile.php\?id=(.+?)&', a['href']).group(1)
+				friend = re.search('https://www.facebook.com/(profile.php\?id=(.+?))&', a['href']).group(1)
 			all+= friend + ";"
 		
 		try:
@@ -54,5 +60,30 @@ while(True):
 		arq.write(all)
 		print("Lista de amigos salva.")
 			
-	elif(op == "compare")
+	elif(op == "intersec"):
+		intersec = open(args[0] + "/friends.txt", 'r').read().split(";")[0:-1]
+
+		for i in range(1, len(args)):
+			intersec = set(open(args[i] + "/friends.txt", 'r').read().split(";")).intersection(intersec)
+		
+		intersec = list(intersec)
+
+		for i in range(0, len(intersec)):
+			print(str(i) + " - " + intersec[i])
+		global_list = intersec
+	elif(op == "browser"):
+		browser = Browser('chrome', options=chrome_options)
+		url = "http://www.facebook.com/login"
+		browser.visit(url)
+	elif(op == "open"):
+		if(args[0] == "all"):
+			for i in global_list:
+				url = "http://www.facebook.com/" + i
+				openTab(url)
+		else:
+			for arg in args:
+				url = "http://www.facebook.com/" + global_list[int(arg)]
+				openTab(url)
+
+		
     
